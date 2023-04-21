f9ea0e55c8a52d1f663f102b7f115c1b7f9ca123
Gustom49 <115970395+Gustom49@users.noreply.github.com>
2023-04-21 10:11:01 +1000

    i did more things

---
+++ webiste/index copy.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <title>Student Notices</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   </head>
+<body> 
+  <nav>
+    <div class="menu">
+      <div class="logo">
+        <a href="https://docs.google.com/document/d/17UX81WGmB5AHQ0CM-rQPmZboYqufqTJ6z2mFbm6EMw4/edit#heading=h.qwf1bzhntltp">moring prayer</a>
+      </div>
+      <ul>
+        <li><a href="https://docs.google.com/document/d/17UX81WGmB5AHQ0CM-rQPmZboYqufqTJ6z2mFbm6EMw4/edit#heading=h.cy11f8o4lhr0">room changes</a></li>
+        <li><a href="https://docs.google.com/document/d/17UX81WGmB5AHQ0CM-rQPmZboYqufqTJ6z2mFbm6EMw4/edit#heading=h.vnyziswzcrhz">clubs and cometys</a></li>
+        <li><a href="https://docs.google.com/document/d/17UX81WGmB5AHQ0CM-rQPmZboYqufqTJ6z2mFbm6EMw4/edit#heading=h.8rl7m1f7th9t">music/drama</a></li>
+        <li><a href="https://docs.google.com/document/d/17UX81WGmB5AHQ0CM-rQPmZboYqufqTJ6z2mFbm6EMw4/edit#heading=h.v47f5y36gzh7">sports</a></li>
+        <li><a href="https://docs.google.com/document/d/17UX81WGmB5AHQ0CM-rQPmZboYqufqTJ6z2mFbm6EMw4/edit#heading=h.8srp94pvkmpb">year 11 notices</a></li>
+      </ul>
+    </div>
+  </nav>
+  
+   
+</body>
+</html>
